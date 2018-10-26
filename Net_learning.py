2a51e9e8a1f6cfd0a7d6aff4d98aedc4e9c3e680
mhhuang95 <mhhuang.ucdavis.edu>
2018-10-25 22:15:39 -0700

    Net

--- Net_learning.py
+++ Net_learning.py
@@ -1,69 +1,76 @@
 import gym
 import gym.spaces
-import random
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+class tf_model():
+    def __init__(self):
+        tf.reset_default_graph()
+        self.inputs = tf.placeholder(shape = [1,16], dtype=tf.float32)
+        self.W = tf.Variable(tf.random_uniform([16,4],0,0.01))
+        self.Qout = tf.matmul(self.inputs, self.W)
+        self.predict = tf.argmax(self.Qout,1)
 
-if __name__ == "__main__":
+        self.next_Q = tf.placeholder(shape=[1,4],dtype=tf.float32)
+        self.loss = tf.reduce_sum(tf.square(self.Qout - self.next_Q))
+        self.trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+        self.update = self.trainer.minimize(self.loss)
+
+        self.init = tf.global_variables_initializer()
+
+
+def Net_learing():
+
     env = gym.make('FrozenLake-v0')
 
-    #the feedforward net
-    tf.reset_default_graph()
-    inputs1 = tf.placeholder(shape=[1,16], dtype=tf.float32)
-    W = tf.Variable(tf.random_uniform([16,4], 0,0.01))
-    Qout = tf.matmul(inputs1, W)
-    predict = tf.argmax(Qout,1)
+    model = tf_model()
 
-    #the loss
-    nextQ = tf.placeholder(shape=[1,4], dtype=tf.float32)
-    loss = tf.reduce_sum(tf.square(nextQ - Qout))
-    trainer = tf.train.GradientDescentOptimizer(learning_rate= 0.1)
-    updateModel = trainer.minimize(loss)
+    gamma = 0.95
 
-    #Train the net
-    init = tf.global_variables_initializer()
-
-    y = 0.99
-    e = 0.1
-    num_episodes = 2000
-    jList = []
+    num_episode = 2000
+    epsilon = 0.1
     rList = []
+    jList = []
+
     with tf.Session() as sess:
-        sess.run(init)
-        for i in range(num_episodes):
+        sess.run(model.init)
+
+        for i in range(num_episode):
             s = env.reset()
+            done = False
             rAll = 0
-            d = False
             j = 0
+
             while j < 99:
                 j = j + 1
 
-                a, allQ = sess.run([predict, Qout], feed_dict={inputs1 : np.identity(16)[s:s+1]})
+                action , Qout = sess.run([model.predict, model.Qout], feed_dict={model.inputs: np.identity(16)[s:s+1]})
+                if np.random.rand(1) < epsilon:
+                    action[0] = env.action_space.sample()
 
-                if np.random.rand(1) < e:
-                    a[0] = env.action_space.sample()
+                s1,r,done,_ = env.step(action[0])
 
-                s1, r, d, _ = env.step(a[0])
-                Q1 = sess.run(Qout, feed_dict= {inputs1: np.identity(16)[s1:s1+1]})
+                rAll += r
+                Q1 = sess.run(model.Qout, feed_dict={model.inputs:np.identity(16)[s1:s1+1]})
 
-                maxQ1 = np.max(Q1)
-                targetQ = allQ
-                targetQ[0,a[0]] = r + y*maxQ1
+                Qmax = np.max(Q1)
+                targetQ = Qout
+                targetQ[0,action[0]] = r + gamma*Qmax
 
-                _, W1 = sess.run([updateModel, W], feed_dict={inputs1:np.identity(16)[s:s+1], nextQ: targetQ})
-                rAll = rAll + r
+                _, W = sess.run([model.update, model.W], feed_dict={model.inputs:np.identity(16)[s:s+1], model.next_Q: targetQ})
+
                 s = s1
-                if d == True:
-
-                    e = 1./((i/50)+10)
+                if done is True:
+                    epsilon = 1. / ((i / 50) + 10)
                     break
-                jList.append(j)
-                rList.append(rAll)
 
-    print("Percent of successful episodes: " + str(sum(rList) / num_episodes) + "%")
+            jList.append(j)
+            rList.append(rAll)
+    print("Percent of succesful episodes: " + str(sum(rList) / num_episode) + "%")
+
     #print(rList)
-
     plt.plot(rList)
-    plt.plot(jList)
+
+if __name__ == "__main__":
+    Net_learing()
