7a5df2c27357d403c96fff3b3b052d2db7ea0607
mhhuang95 <mhhuang.ucdavis.edu>
2018-10-25 22:14:39 -0700

    bandit

--- bandit.py
+++ bandit.py
@@ -1,59 +1,62 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
-# define the bandits
+
 bandits = [0.2, 0, -0.2, -5]
 num_bandits = len(bandits)
 
-def pullBandits(bandit):
-    result = np.random.randn(1)
-    if result > bandit:
+
+def pull_bandit(action):
+    r = np.random.randn(1)
+    if r > bandits[action]:
         return 1
     else:
         return -1
 
 
-if __name__ == "__main__":
-    tf.reset_default_graph()
-    #feed forward
-    weights = tf.Variable(tf.ones([num_bandits]))
-    chosen_action = tf.argmax(weights,0)
+class tf_model():
 
-    #training process
-    reward_holder = tf.placeholder(shape=[1], dtype=tf.float32)
-    action_holder = tf.placeholder(shape=[1], dtype=tf.int32)
-    responsible_weight = tf.slice(weights, action_holder, [1])
-    loss = -(tf.log(responsible_weight)*reward_holder)
-    optimezer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    update = optimezer.minimize(loss)
+    def __init__(self):
+        tf.reset_default_graph()
+        self.W = tf.Variable(tf.ones([num_bandits]))
+        self.choose_a = tf.argmax(self.W,0)
 
-    #training
-    total_episode = 1000
-    total_reward = np.zeros(num_bandits)
-    e = 0.1
+        self.reward_holder = tf.placeholder(shape=[1], dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[1], dtype=tf.int32)
+        self.responsible_weight = tf.slice(self.W, self.action_holder, [1])
+        self.loss = - tf.log(self.responsible_weight)*self.reward_holder
+        self.trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+        self.update = self.trainer.minimize(self.loss)
+        self.init = tf.global_variables_initializer()
 
-    init = tf.global_variables_initializer()
+
+def run():
+
+    model = tf_model()
+
+    num_episode = 2000
+    epsilon = 0.1
+    total_reward = np.zeros([len(bandits)])
 
     with tf.Session() as sess:
-        sess.run(init)
-        i = 0
-        while i < total_episode:
-            if np.random.rand(1) < e:
-                action = np.random.randint(num_bandits)
+        sess.run(model.init)
+
+        for i in range(num_episode):
+            if np.random.uniform() > epsilon:
+                action = sess.run(model.choose_a)
             else:
-                action = sess.run(chosen_action)
+                action = np.random.choice(len(bandits))
 
-            reward = pullBandits(bandits[action])
+            reward = pull_bandit(action)
 
-            _, resp, ww = sess.run([update, responsible_weight, weights], feed_dict={reward_holder: [reward], action_holder: [action]})
-            total_reward[action] = total_reward[action] + reward
+            _, resp, weights = sess.run([model.update, model.responsible_weight, model.W], feed_dict={model.reward_holder: [reward], model.action_holder: [action]})
+
+            total_reward[action] += reward
+
             if i % 50 == 0:
-                print
-                "Running reward for the " + str(num_bandits) + " bandits: " + str(total_reward)
-            i += 1
-    print("The agent thinks bandit " + str(np.argmax(ww) + 1) + " is the most promising....")
-    print(ww)
-    if np.argmax(ww) == np.argmax(-np.array(bandits)):
-        print("...and it was right!")
-    else:
-        print("...and it was wrong!")
+                print("Running reward for all bandits is", str(total_reward))
+        print(weights)
+
+
+if __name__ == "__main__":
+    run()
